f7e56091fbbf8216d4a1f830d76428f0b6b74a02
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-04-16 21:44:01 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,18 @@
 -- Use SQL insert statements to add any
 -- starting/dummy data to your database tables
 
--- EXAMPLE:
+insert into "users"
+    ("username", "passwordHash")
+  values
+    ('Brian', 'Wilson');
 
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+insert into "filterSets"
+    ("label", "userId", "name", "phoneNumber",
+    "emailAddress", "dateTime", "location",
+    "usSsn", "usDriversLicense", "cryptoWalletNumber",
+    "usBankNumber")
+  values
+    ('schedules1', 1, false, true,
+    true, true, true,
+    false, false, false,
+    false);
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY NOT NULL,
+  "username" text NOT NULL,
+  "passwordHash" text NOT NULL,
+  "createdAt" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "filterSets" (
+  "filterSetId" serial PRIMARY KEY NOT NULL,
+  "label" text NOT NULL,
+  "userId" integer NOT NULL,
+  "name" boolean NOT NULL,
+  "phoneNumber" boolean NOT NULL,
+  "emailAddress" boolean NOT NULL,
+  "dateTime" boolean NOT NULL,
+  "location" boolean NOT NULL,
+  "usSsn" boolean NOT NULL,
+  "usDriversLicense" boolean NOT NULL,
+  "cryptoWalletNumber" boolean NOT NULL,
+  "usBankNumber" boolean NOT NULL,
+  "createdAt" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "filterSets" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
