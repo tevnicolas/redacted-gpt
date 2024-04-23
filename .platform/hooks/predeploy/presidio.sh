46da71e9874b04c9cbcf80e23135bd3fb779647b
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-04-23 23:49:54 +0000

    Further changes.

--- .platform/hooks/predeploy/presidio.sh
+++ .platform/hooks/predeploy/presidio.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+echo Installing pip
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 
+echo Installing presidio_analyzer
+
 pip install presidio_analyzer
+
+echo Installing presidio_anonymizer
+
 pip install presidio_anonymizer
+
+echo download en_core_web_lg
+
 python3 -m spacy download en_core_web_lg
